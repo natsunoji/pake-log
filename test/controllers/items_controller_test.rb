c1ce8a4fbf2e456ffb4471a5889aaa68883d8623
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-03-22 00:11:22 +0900

    パケ登録・一覧表示機能の実装とUI/テストの最終調整

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,18 +1,35 @@
 require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    # users.yml で定義した 'standard_user' を使う
+    @user = users(:standard_user)
+    sign_in @user
+  end
+
   test "should get index" do
-    get items_index_url
+    get items_url
     assert_response :success
   end
 
   test "should get new" do
-    get items_new_url
+    get new_item_url
     assert_response :success
   end
 
-  test "should get create" do
-    get items_create_url
-    assert_response :success
+  test "should create item" do
+    assert_difference("Item.count") do
+      post items_url, params: {
+        item: {
+          name: "新規パケ登録テスト",
+          item_images_attributes: [
+            { image: fixture_file_upload("test/fixtures/files/test_image.png", "image/png") }
+          ]
+        }
+      }
+    end
+    assert_redirected_to items_url
   end
 end
