647eb51788bdee8d280a8e1fd9b250f876f1a9aa
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-03-22 11:43:29 +0900

    画像のメタデータ削除およびリサイズ機能の共通化

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -4,4 +4,14 @@ class ItemImage < ApplicationRecord
   has_one_attached :image
 
   validates :image, presence: true
+
+  # 引数 resize_limit でサイズを指定できるように変更。デフォルトは [400, 400]
+  def display_image(resize_limit = [ 400, 400 ])
+    if image.variable?
+      # resize_to_limit を追加することで、指定サイズに収まるようリサイズしつつメタデータを削除
+      image.variant(resize_to_limit: resize_limit, saver: { strip: true }).processed
+    else
+      image
+    end
+  end
 end
