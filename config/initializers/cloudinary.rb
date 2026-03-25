f8d46ea9763606adb139ae87e2044313569ba2a7
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-03-25 23:35:48 +0900

    画像選択の保持機能の追加とCloudinary表示エラーの解消

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV["dvr7s1wdd"]
+  config.api_key    = ENV["986751225416756"]
+  config.api_secret = ENV["57xBj59klIE9In0fYckBd-R09OM"]
+  config.secure     = true
+end
