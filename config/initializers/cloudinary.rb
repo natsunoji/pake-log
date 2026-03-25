6d2c78d729fd66d4c8778d449a3d324dee4c459d
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-03-26 01:21:02 +0900

    Fix Cloudinary configuration

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,1 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV["dvr7s1wdd"]
-  config.api_key    = ENV["986751225416756"]
-  config.api_secret = ENV["57xBj59klIE9In0fYckBd-R09OM"]
-  config.secure     = true
-end
+Cloudinary.config_from_url(ENV["CLOUDINARY_URL"])
