dcff33ea0aa81ad9c9af77ae1ebd7afa073adcef
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-03-29 16:35:19 +0900

    Fix: image deletion logic and mobile UI overlap

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-  # index と new 以外（つまり特定の1件を扱うアクション）で set_item を動かす
   before_action :set_item, only: %i[show edit update destroy]
 
   def index
@@ -11,7 +10,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # before_action で @item がセットされているので中身は空でOK
   end
 
   def new
@@ -24,7 +22,6 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to item_path(@item), notice: "登録しました"
     else
-      # エラー時はnew画面を再表示
       render :new, status: :unprocessable_entity
     end
   end
@@ -33,16 +30,38 @@ class ItemsController < ApplicationController
   end
 
   def update
-    filtered_params = item_params
-    if params[:item][:images].blank? || params[:item][:images].all?(&:blank?)
-      filtered_params = filtered_params.except(:images)
-    end
+    # 🌟 トランザクション処理で、画像削除・追加・基本情報の更新をセットで行う
+    ActiveRecord::Base.transaction do
+      # 1. 既存画像の削除処理
+      if params[:item][:delete_image_ids].present?
+        params[:item][:delete_image_ids].each do |image_id|
+          image = @item.images.find_by(id: image_id)
+          image.purge if image
+        end
+      end
 
-    if @item.update(filtered_params)
-      redirect_to item_path(@item), notice: "更新しました"
-    else
-      render :edit, status: :unprocessable_entity
+      # 2. 新規画像の追加処理（JS側で3枚制限されているが、念のためサーバー側でもガード）
+      if params[:item][:images].present?
+        new_images = params[:item][:images].reject(&:blank?)
+        available_slots = 3 - @item.images.count
+
+        if available_slots > 0
+          images_to_attach = new_images.first(available_slots)
+          @item.images.attach(images_to_attach)
+        end
+      end
+
+      # 3. 基本情報の更新（images, delete_image_ids は既に処理済みなので除外）
+      if @item.update(item_params.except(:images, :delete_image_ids))
+        redirect_to item_path(@item), notice: "更新しました"
+      else
+        # バリデーションエラー時はトランザクションをロールバックさせる
+        raise ActiveRecord::Rollback
+      end
     end
+  rescue ActiveRecord::Rollback
+    # ロールバック後に編集画面を再表示
+    render :edit, status: :unprocessable_entity
   end
 
   def destroy
@@ -56,13 +75,12 @@ class ItemsController < ApplicationController
   private
 
   def set_item
-    # 🌟 ログインしている自分の説明書の中から探す
     @item = current_user.items.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to items_path, alert: "指定されたアイテムが見つかりません、または権限がありません。"
   end
 
   def item_params
-    params.require(:item).permit(:name, :category_id, :memo, images: [])
+    params.require(:item).permit(:name, :category_id, :memo, images: [], delete_image_ids: [])
   end
 end
--- app/javascript/controllers/input_images_controller.js
+++ app/javascript/controllers/input_images_controller.js
@@ -7,13 +7,24 @@ export default class extends Controller {
     "placeholderIcon", 
     "count", 
     "removeButton",
-    "coverLabel",      // 🌟 追加
-    "previewBox"       // 🌟 追加
+    "coverLabel",
+    "previewBox"
   ]
 
   connect() {
-    this.allFiles = [] // 選択したファイルを溜めておく配列
-    this.updateCount() // 初期状態のカウントを表示
+    this.allImages = []
+    this.newFiles = []
+    this.deletedIds = []
+    
+    // 既存画像を読み込み
+    this.previewBoxTargets.forEach((box) => {
+      const img = box.querySelector('[data-input-images-target="image"]')
+      if (img && img.src && !img.classList.contains('hidden')) {
+        const imageId = box.querySelector('[data-image-id]')?.dataset.imageId
+        this.allImages.push({ type: 'persisted', id: imageId, src: img.src })
+      }
+    })
+    this.renderAll()
   }
 
   triggerFile() {
@@ -30,71 +41,108 @@ export default class extends Controller {
     const newFiles = Array.from(this.inputTarget.files)
     if (newFiles.length === 0) return
 
-    // 既存のファイルに新しいファイルを追加（最大3枚まで）
-    this.allFiles = [...this.allFiles, ...newFiles].slice(0, 3)
-    
-    this.renderPreviews()
-    this.syncToInput() // 重要：inputの中身を最新の状態に同期する
-  }
+    const totalCount = this.allImages.length + this.newFiles.length
+    const availableSlots = 3 - totalCount
+    if (availableSlots <= 0) return
 
-  renderPreviews() {
-    // 一旦リセット
-    this.imageTargets.forEach((img, i) => {
-      img.classList.add("hidden")
-      img.src = ""
-      this.placeholderIconTargets[i].classList.remove("hidden")
-      
-      // 削除ボタンを非表示
-      if (this.hasRemoveButtonTarget && this.removeButtonTargets[i]) {
-        this.removeButtonTargets[i].classList.add("hidden")
-      }
-      
-      // 🌟 カバー画像のラベルを非表示
-      if (i === 0 && this.hasCoverLabelTarget) {
-        this.coverLabelTarget.classList.add("hidden")
-      }
-    })
-
-    // 配列の中身を表示
-    this.allFiles.forEach((file, i) => {
-      const reader = new FileReader()
-      reader.onload = (e) => {
-        this.imageTargets[i].src = e.target.result
-        this.imageTargets[i].classList.remove("hidden")
-        this.placeholderIconTargets[i].classList.add("hidden")
-        
-        // 削除ボタンを表示
-        if (this.hasRemoveButtonTarget && this.removeButtonTargets[i]) {
-          this.removeButtonTargets[i].classList.remove("hidden")
-        }
-        
-        // 🌟 1枚目の画像にカバー画像のラベルを表示
-        if (i === 0 && this.hasCoverLabelTarget) {
-          this.coverLabelTarget.classList.remove("hidden")
-        }
-      }
-      reader.readAsDataURL(file)
-    })
+    const filesToAdd = newFiles.slice(0, availableSlots)
+    this.newFiles = [...this.newFiles, ...filesToAdd]
     
-    this.updateCount() // カウントを更新
+    this.renderAll()
+    this.syncToInput()
   }
 
   remove(e) {
     const index = parseInt(e.currentTarget.dataset.index)
-    this.allFiles.splice(index, 1) // 配列から削除
-    this.renderPreviews()
+    const allData = this.getAllImagesData()
+    const imageData = allData[index]
+    
+    if (!imageData) return
+
+    if (imageData.type === 'persisted') {
+      this.deletedIds.push(imageData.id)
+      this.allImages = this.allImages.filter(img => img.id !== imageData.id)
+    } else {
+      const newFileIndex = imageData.fileIndex
+      this.newFiles.splice(newFileIndex, 1)
+    }
+    
+    this.renderAll()
     this.syncToInput()
+    this.syncDeletedIds()
   }
 
-  // フォーム送信時にこの allFiles の中身が送られるようにする
+  getAllImagesData() {
+    const result = []
+    this.allImages.forEach(img => result.push({ type: 'persisted', id: img.id, src: img.src }))
+    this.newFiles.forEach((file, index) => result.push({ type: 'new', file: file, fileIndex: index }))
+    return result
+  }
+
+  renderAll() {
+    const allImagesData = this.getAllImagesData()
+    
+    this.previewBoxTargets.forEach((box, index) => {
+      const img = box.querySelector('[data-input-images-target="image"]')
+      const icon = box.querySelector('[data-input-images-target="placeholderIcon"]')
+      const btn = box.querySelector('[data-input-images-target="removeButton"]')
+      const cover = box.querySelector('[data-input-images-target="coverLabel"]')
+      
+      const data = allImagesData[index]
+      
+      if (data) {
+        if (btn) {
+          btn.dataset.index = index
+          btn.classList.remove('hidden')
+        }
+        
+        if (data.type === 'persisted') {
+          img.src = data.src
+          img.classList.remove('hidden')
+          icon?.classList.add('hidden')
+        } else {
+          const reader = new FileReader()
+          reader.onload = (e) => {
+            img.src = e.target.result
+            img.classList.remove('hidden')
+            icon?.classList.add('hidden')
+          }
+          reader.readAsDataURL(data.file)
+        }
+        
+        if (cover) {
+          index === 0 ? cover.classList.remove('hidden') : cover.classList.add('hidden')
+        }
+      } else {
+        // 🌟 画像がない場合：アイコンを復活させる
+        img.src = ''
+        img.classList.add('hidden')
+        icon?.classList.remove('hidden')
+        btn?.classList.add('hidden')
+        cover?.classList.add('hidden')
+      }
+    })
+    this.updateCount()
+  }
+  
+  syncDeletedIds() {
+    this.element.querySelectorAll('input[name="item[delete_image_ids][]"]').forEach(input => input.remove())
+    this.deletedIds.forEach(id => {
+      const input = document.createElement('input')
+      input.type = 'hidden'
+      input.name = 'item[delete_image_ids][]'
+      input.value = id
+      this.element.appendChild(input)
+    })
+  }
+
   syncToInput() {
     const dataTransfer = new DataTransfer()
-    this.allFiles.forEach(file => dataTransfer.items.add(file))
+    this.newFiles.forEach(file => dataTransfer.items.add(file))
     this.inputTarget.files = dataTransfer.files
   }
 
-  // カウントを更新
   updateCount() {
-    this.countTarget.textContent = this.allFiles.length
+    this.countTarget.textContent = this.allImages.length + this.newFiles.length
   }
 }
