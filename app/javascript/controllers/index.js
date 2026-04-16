f55b28ffa1c0a2ae0cb832afdba163c79a26db6a
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-04-16 15:14:33 +0900

    fix: importパスを相対パスから識別子に変更し、本番の404エラーを解消

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,30 +1,27 @@
 // app/javascript/controllers/index.js
+import { application } from "controllers/application"
 
-import { application } from "./application"
-
-// 🌟 404の原因となる「自動探索(eagerLoad)」は一切使わない。
-// 🌟 必要なコントローラーを一つずつ「明示的」に登録する。
-
-import SortableController from "./sortable_controller"
-application.register("sortable", SortableController)
-
-import CarouselController from "./carousel_controller"
+// 🌟 "./" を消して、importmap.rb で定義した名前に書き換える
+import CarouselController from "controllers/carousel_controller"
 application.register("carousel", CarouselController)
 
-import FlashController from "./flash_controller"
+import FlashController from "controllers/flash_controller"
 application.register("flash", FlashController)
 
-import HelloController from "./hello_controller"
+import HelloController from "controllers/hello_controller"
 application.register("hello", HelloController)
 
-import ImageModalController from "./image_modal_controller"
+import ImageModalController from "controllers/image_modal_controller"
 application.register("image-modal", ImageModalController)
 
-import ImagePreviewController from "./image_preview_controller"
+import ImagePreviewController from "controllers/image_preview_controller"
 application.register("image-preview", ImagePreviewController)
 
-import InputImagesController from "./input_images_controller"
+import InputImagesController from "controllers/input_images_controller"
 application.register("input-images", InputImagesController)
 
-import PasswordVisibilityController from "./password_visibility_controller"
+import PasswordVisibilityController from "controllers/password_visibility_controller"
 application.register("password-visibility", PasswordVisibilityController)
+
+import SortableController from "controllers/sortable_controller"
+application.register("sortable", SortableController)
