0977e2c0b6fae8933ae745cc10f7e3eb13a5903e
natsunoji <127575383+natsunoji@users.noreply.github.com>
2026-04-16 15:05:26 +0900

    fix: カテゴリ編集画面の復元と本番環境でのJSロードエラー(404)の修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,30 @@
+// app/javascript/controllers/index.js
+
 import { application } from "./application"
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// 🌟 404の原因となる「自動探索(eagerLoad)」は一切使わない。
+// 🌟 必要なコントローラーを一つずつ「明示的」に登録する。
+
+import SortableController from "./sortable_controller"
+application.register("sortable", SortableController)
+
+import CarouselController from "./carousel_controller"
+application.register("carousel", CarouselController)
+
+import FlashController from "./flash_controller"
+application.register("flash", FlashController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import ImageModalController from "./image_modal_controller"
+application.register("image-modal", ImageModalController)
+
+import ImagePreviewController from "./image_preview_controller"
+application.register("image-preview", ImagePreviewController)
+
+import InputImagesController from "./input_images_controller"
+application.register("input-images", InputImagesController)
+
+import PasswordVisibilityController from "./password_visibility_controller"
+application.register("password-visibility", PasswordVisibilityController)
